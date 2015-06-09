badb35d3707db63f6146ebe4c6283c07602ff2cb
Marcin Kosiński <m.p.kosinski@gmail.com>
2015-06-10 00:10:35 +0200

    Update propozycje_aplikacji.R

--- Faza3/propozycje_aplikacji.R
+++ Faza3/propozycje_aplikacji.R
@@ -1,13 +1,30 @@
 #ktos wybiera film i !
-   i <- 150
+   i <- 350
 
 # mamy dla niego najbardziej podobne filmy
-which( moviesInfo[150,] > 0 ) -> numerki_podobnych
+which( moviesInfo[i,] > 0 ) -> numerki_podobnych
 
-# i teraz pomniejszamy zbior biora tylko te wiersze i kolumy
+n<-5
+sapply(1:n, function(element){
+   which( element == order(moviesInfo[i,numerki_podobnych]))
+}) -> najblizsze_n
 
 
 
-moviesInfo[ numerki_podobnych, numerki_podobnych] -> podobne_do_i
+# dla najblizszych tez bierzemy 5 najbardziej podobnych
+sapply(numerki_podobnych[najblizsze_n], function(element){
+   numerki_podobnych_2 <- which( moviesInfo[element,] > 0 )
+   sapply(1:n, function(element2){
+      which( element2  == order(moviesInfo[element,numerki_podobnych_2]))
+   }
+   ) -> x_numerki
+   numerki_podobnych_2[x_numerki]
+   
+}
+) -> numeraski
+unique(as.vector(numeraski)) -> numeraski2
+
+moviesInfo[ c(i,numerki_podobnych[najblizsze_n], numeraski2),
+            c(i,numerki_podobnych[najblizsze_n], numeraski2)] -> podobne_do_i
 
 # no i rysujemy tylko taka macierz, gdzie jakos trzeba oznaczyc ten i-ty film
