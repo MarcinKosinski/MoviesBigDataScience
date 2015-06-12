b8869c8fc4835adcbf1f77259281e4897899eb55
MarcinKosinski <kosinskim@student.mini.pw.edu.pl>
2015-06-12 13:01:00 +0200

    ulepszony ui: dodani autorzy i opis grafu

--- Faza3/shiny/ui.R
+++ Faza3/shiny/ui.R
@@ -16,7 +16,7 @@ names(tytul) <- stri_replace_all_regex(movies$title, "\\032", "")
 tytul <- stri_replace_all_regex(movies$title, "\\032", "")
 
 shinyUI(fluidPage(
-   titlePanel("Projekt filmy"),
+   titlePanel("Serwis Rekomendujący Filmy"),
    sidebarLayout(sidebarPanel("",
           # wybór filmu
          selectInput("movie","Wybierz film:",tytul, "50 twarzy Greya"),
@@ -32,7 +32,8 @@ shinyUI(fluidPage(
          selectInput("ile_podobnych_do_najpodobniejszych",
                      "Wybierz liczbe podobnych filmów do podobnych:",0:5, 2),
          sliderInput("odleglosc",
-                     "Przemnoż odległości w grafie :",min=1,max=10, value = 50)
+                     "Przemnoż odległości w grafie :",min=1,max=10, value = 50),
+         h5("Autorzy serwisu: Marcin Kosiński, Mikołaj Waśniowski, Paweł Grabowski.")
          ),
          
          
@@ -51,8 +52,15 @@ shinyUI(fluidPage(
             tabPanel("Podobne filmy",
                      p("Lista podobnych filmów:"),
                      verbatimTextOutput("podobne_lista"),
+                     p("Opis grafu:"),
+                     h5("Każdzy wierzchołek grafu reprezentuje jeden film. Na grafie ciemnym niebieskim kolorem zaznaczony jest wybrany film. Dla danego filmu pokazana jest wybrana liczba filmów, które
+                        są do niego najbardziej podobne. Długość krawędzi łącząca wierzchołki w grafie odpowiada podobieństwu filmów: im krótsza krawędź łącząca wierzchołki, tym filmy są bardziej do siebie 
+                        podobne. Dla podobnych filmów do wybranego filmu, przedstawione są również filmy najbardziej podobne do nich. Dla tak wybranych sąsiadów sąsiadów, jeżeli pozostałe filmy są wśród 20 najbardziej
+                        podobnych filmów do nich, to również są połączeni krawędzią, której długość odpowiada podobieństwu filmów. Kolory na grafie odpowiadają tematyce filmów. Jeżeli wierzchołki mają ten sam
+                        kolor, to znaczy, że filmy, które odpowiadają wierzchołkom pochodzą z tej samej tematyki."),
                      p("Graf podobnych filmów:"),
                      forceNetworkOutput("graph")
+                     
             )
             
          )
